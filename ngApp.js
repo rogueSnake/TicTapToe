be9403fea9a0cabed08aa8ad7b15ebd61de30f25
rogueSnake <ekanseugor@gmail.com>
2015-01-05 13:09:43 -0600

    Trimmed fat from js, added comment to html.

--- ngApp.js
+++ ngApp.js
@@ -3,33 +3,28 @@ TTT.app = angular.module('app', []);
 
 TTT.app.controller('appCtrl', function ($scope) {
     var i = 0,
-        row = {},
         protoRow =  {
         columnA: "_",
         columnB: "_",
         columnC: "_"
     };
 
+    $scope.grid = [];
+
     for (i=0; i < 3; i += 1) {
-        row[i] = Object.create(protoRow);
+        $scope.grid.push(Object.create(protoRow));
     }
 
-    $scope.grid = [
-        row[0],
-        row[1],
-        row[2]
-    ];
-
     $scope.mod = function (row, column) {
-        
+
         if ($scope.grid[row][column] === "_"){
             $scope.grid[row][column] = "x";
         }
-        
+
         else if ($scope.grid[row][column] === "x"){
             $scope.grid[row][column] = "o";
         }
-        
+
         else {
             $scope.grid[row][column] = "_";
         }
